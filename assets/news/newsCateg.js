11c8c601dcddf03a201000f7a2447d1954b367e2
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-05 15:36:25 +0100

    Update newsCateg.js

--- assets/news/newsCateg.js
+++ assets/news/newsCateg.js
@@ -13,4 +13,10 @@ export const newsCateg = [{
     header: 'Monday 5pm, womens prayer',
     description: 'A medium for women to come together and pray for their families and love ones',
     date: 'Sep 29, 2025'
+}, {
+    id: '538989hj629dhbshfyeb8495jdj82hf',
+    header: 'Alleluia coming up this December',
+    description: 'A Vibrant community of faith, spreading love, hope, and the gospel of Christ, an event for worship and prayer',
+    date: 'December 2025'
 }]
+
