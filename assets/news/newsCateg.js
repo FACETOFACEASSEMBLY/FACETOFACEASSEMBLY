ca6ab648c41dd1077862a4866dcda58fd5b88c2b
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-12 14:57:11 +0100

    Update newsCateg.js

--- assets/news/newsCateg.js
+++ assets/news/newsCateg.js
@@ -1,14 +1,4 @@
 export const newsCateg = [{
-    id: '7384hfkjabbf83bf8af',
-    header: 'Commissioning of Free School & Orphanage Home',
-    description: 'Face to Face Assembly commissioned Jesus The Son of God Educational Center and Ebenezer Orphanage Home to support the community.',
-    date: 'Jan 1, 2024'
-}, {
-    id: '53629dhbshfyeb82hf',
-    header: 'Annual September Remembrance',
-    description: 'We held our September remembrance program with thanksgiving and testimonies.',
-    date: 'Sep 10, 2023'
-}, {
     id: '538989hj629dhbshfyeb82hf',
     header: 'Monday 5pm, womens prayer',
     description: 'A medium for women to come together and pray for their families and love ones',
@@ -19,4 +9,5 @@ export const newsCateg = [{
     description: 'A Vibrant community of faith, spreading love, hope, and the gospel of Christ, an event for worship and prayer',
     date: 'December 2025'
 }]
+
 
