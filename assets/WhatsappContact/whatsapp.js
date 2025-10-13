8647b1f4eb2e423d3e6c67550eacf71b615de59d
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-13 01:00:07 +0100

    Update whatsapp.js

--- assets/WhatsappContact/whatsapp.js
+++ assets/WhatsappContact/whatsapp.js
@@ -2,12 +2,13 @@ function whatsapp() {
     const name = document.getElementById('name').value;
     const message = document.getElementById('message').value;
 
-    const whatsappURL = 'https://wa.me/2348105630089/?text='
+    const whatsappURL = 'https://wa.me/2348033884008/?text='
     + 'Name: ' + name + '%0a'
-    + 'Message: ' + message;
+    + message;
 
     window.open(whatsappURL, '_blank').focus();
 }
 
 const sendMessage = document.getElementById('send');
+
 sendMessage.addEventListener('click', whatsapp);
