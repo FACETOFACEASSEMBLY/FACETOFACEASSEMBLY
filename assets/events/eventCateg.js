63859e7261c32b5ae994f53592a597f93237d5eb
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-18 21:37:52 +0100

    Update eventCateg.js

--- assets/events/eventCateg.js
+++ assets/events/eventCateg.js
@@ -14,7 +14,39 @@ export const eventCateg = [{
         day: 20,
         month: 'OCT'
     }
-},  {
+}, {
+    header: `Morning Prayer`,
+    time: '6:00 AM - 7:00 AM',
+    venue: 'Headquarters Every Tuesday - Bring your prayer Requests',
+    date: {
+        day: 21,
+        month: 'OCT'
+    }
+} ,{
+    header: `Wednesday Evening Prayer`,
+    time: '5:00 PM - 6:00 PM',
+    venue: 'Headquarters Every Wednesday - Bring your prayer Requests',
+    date: {
+        day: 22,
+        month: 'OCT'
+    }
+} ,{
+    header: `Morning Prayer`,
+    time: '6:00 AM - 7:00 AM',
+    venue: 'Headquarters Every Thursday - Bring your prayer Requests',
+    date: {
+        day: 23,
+        month: 'OCT'
+    }
+} ,{
+    header: `Morning Prayer`,
+    time: '6:00 AM - 7:00 AM',
+    venue: 'Headquarters Saturday - Bring your prayer Requests',
+    date: {
+        day: 25,
+        month: 'OCT'
+    }
+} ,{
     header: 'First Day of The Month Prayer',
     time: '5:00 AM Shell road - 6:00 AM Headquarters,Salubi,Vitafoam ',
     venue: 'Declare the month into your hands, and pray for the wellbeing of your family, Join us! and Pray',
