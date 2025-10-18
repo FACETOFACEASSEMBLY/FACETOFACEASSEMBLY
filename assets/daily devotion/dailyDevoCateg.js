33c0e8841e0e213dd787d51ee4eead200444578b
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-18 21:26:56 +0100

    Add files via upload

---
+++ assets/daily devotion/dailyDevoCateg.js
@@ -0,0 +1,42 @@
+export const dailyDevotionCateg = [{
+    date: 'FRIDAY, 17TH OCTOBER 2025',
+    prayer: `<p>MOVE WITH SPEED
+
+“To obey is better than sacrifice.” — 1 Samuel 15:22
+
+When God gives an instruction, He expects immediate action. Delayed obedience is still disobedience, and it often leads to missed opportunities. Noah, Moses, and Esther succeeded because they acted without delay. Jesus also said, “What you do, do quickly” (John 13:27).
+
+Delay gives room for doubt and the enemy’s interference. True obedience requires speed, and it is the key to favor, breakthrough, and divine backing.
+<br>
+<br>
+PRAYER: <br>
+My Father, My God, I receive grace to be upright and make no more delay in my assignment, in Jesus’ Name, Amen.</p>`
+}, {
+    date: 'THURSDAY, 16TH OCTOBER 2025',
+    prayer: `<p>THE EARTH WILL NOT RISE UP AGAINST ME
+
+“The heaven shall reveal his iniquity; and the earth shall rise up against him.” — Job 20:27
+
+The earth is not just soil; it is a living witness. It records, speaks, and can rise in judgment when sin or blood cries out (Genesis 4:10; Jeremiah 22:29–30). While God desires the earth to bless and support His children (Psalm 67:5–6; Revelation 12:16), hidden iniquity can cause it to testify against us.
+
+Only the blood of Jesus silences the accusations of the earth. His blood speaks mercy, cleansing, and restoration (Matthew 26:28; Hebrews 12:24). Through Christ, the earth will no longer rise against you but will help, defend, and favour you.
+<br>
+<br>
+            PRAYER <br>
+My Father, my God, by the blood of Jesus, let every iniquity written or speaking against me from the earth be blotted out. From today, let the earth help me, defend me, and favour me, in Jesus’ mighty name. Amen.
+</p>`
+}, {
+    date: 'WEDNESDAY, 15TH OCTOBER 2025',
+    prayer: `<p>HELPERS FROM THE FOUR GATES OF THE EARTH
+
+When God told Abraham to look north, south, east, and west, it was a sign that His help and blessing can come from every direction. God never runs out of channels to lift a man. He often uses human vessels—like Aaron and Hur for Moses, Joseph for nations, and Barnabas for Paul—to carry His help.
+
+Scripture reveals four major directions with gates (Ezekiel 48:30–35; Revelation 21:12–13), showing that divine help and favor can flow from every side. Just as God sent angels, ravens, and wise men in the past, He can stir hearts from unexpected places to favor you.
+
+Above all, Christ is the ultimate Gate (John 10:9). When you walk with Him, every gate will open, and every helper appointed to your destiny will locate you.
+<br>
+<br>
+PRAYER <br>
+My Father, my God, open to me and my family the four gates, and let helpers and Your blessings flow to us, in Jesus’ name. Amen.
+</p>`
+}]
