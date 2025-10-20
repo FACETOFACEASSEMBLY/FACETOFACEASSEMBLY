909ddd0e4e83ff573d05761d245cc7955701f497
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-20 01:40:37 +0100

    Update eventCateg.js

--- assets/events/eventCateg.js
+++ assets/events/eventCateg.js
@@ -1,20 +1,4 @@
 export const eventCateg = [{
-    header: 'Sunday Worship Service',
-    time: 'First service: 6:30 AM - Second service: 8:30 AM',
-    venue: 'Church Auditorium - Join us for worship, prayer, and a powerful word of God. All are welcome!',
-    date: {
-        day: 19,
-        month: 'OCT'
-    } 
-},{
-    header: `Women's Prayer`,
-    time: '5:00 PM',
-    venue: 'Prayer Hall Every Monday - Bring your prayer Requests',
-    date: {
-        day: 20,
-        month: 'OCT'
-    }
-}, {
     header: `Morning Prayer`,
     time: '6:00 AM - 7:00 AM',
     venue: 'Headquarters Every Tuesday - Bring your prayer Requests',
