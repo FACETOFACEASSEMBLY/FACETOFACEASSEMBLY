fcf60638f86c7bfdf96375ee978c0322af03451b
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-22 00:37:50 +0100

    Update newsCateg.js

--- assets/news/newsCateg.js
+++ assets/news/newsCateg.js
@@ -6,12 +6,16 @@ export const newsCateg = [{
 }, {
     id: '538989hj629dhbshfyeb8495jdj82hf',
     header: 'Alleluia coming up this December',
-    description: 'A Vibrant community of faith, spreading love, hope, and the gospel of Christ, an event for worship and prayer',
+    description: 'A Vibrant community of faith, spreading love, hope, and the gospel of Christ, an event for worship and prayer.',
     date: 'December 2025'
 }, {
     id: '538989hj629dhbshsdsdfyeb8495jdj82hf',
     header: 'Cancellation of Womens Prayer this Week',
-    description: 'On the 19th October, it was announced that doing this monday Womens prayer would be cancelled',
+    description: 'On the 19th October, it was announced that doing this monday Womens prayer would be cancelled.',
+    date: 'October 2025'
+}, {
+    id: '538989hj629dhbstui79rthkdhsdsdfyeb8495jdj82hf',
+    header: `RESUMPTION OF WOMEN'S PRAYER NEXT WEEK 27TH OCTOBER`,
+    description: 'On the 20th October, the resumption of womens prayer will hold on next week 27th October, till futher notice.',
     date: 'October 2025'
 }]
-
