65ffc9b1fd228033afb3c93dbeefec22fe51336c
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-22 15:41:57 +0100

    Update facebookCateg.js

--- assets/Facebook/facebookCateg.js
+++ assets/Facebook/facebookCateg.js
@@ -1,5 +1,11 @@
 export const facebookCateg = [{
-    facebookVid: `<iframe  src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fvideos%2F1603452520617684%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe><p class="sermon-date"> October 19, 2025, SUNDAY SERVCIE</p>`
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1827291111214457%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+      <p class="sermon-date"> October 21, 2025</p>`
+}, {
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1266991678448831%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+      <p class="sermon-date"> October 21, 2025</p>`
+}, {
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F851203370574160%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe><p class="sermon-date"> October 20, 2025, SUNDAY SERVCIE</p>`
 }, {
     facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fvideos%2F806931648615192%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
       <p class="sermon-date"> October 19, 2025</p>`
