7b28499a772b5d46ce2558244819142c766f62fc
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-25 10:24:42 +0100

    Update facebookCateg.js

--- assets/Facebook/facebookCateg.js
+++ assets/Facebook/facebookCateg.js
@@ -1,4 +1,7 @@
 export const facebookCateg = [{
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1166425608244703%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+      <p class="sermon-date"> October 24, 2025</p>`
+},{
     facebookVid: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fposts%2Fpfbid02EQnuGpRnEr9cBvZ77vMfHReqnrrG3ZTxzhhySGcTXt1G7X4ohP4ERAhDCHTGjbFKl&show_text=true&width=500" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
       <p class="sermon-date"> October 24, 2025</p>`
 },{
