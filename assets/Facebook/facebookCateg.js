6af7fa019e626e9a31e6690262483f5030fce0b7
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-27 14:32:55 +0000

    Update facebookCateg.js

--- assets/Facebook/facebookCateg.js
+++ assets/Facebook/facebookCateg.js
@@ -1,9 +1,9 @@
 export const facebookCateg = [{
-    facebookVid: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fposts%2Fpfbid0wcK4FMVBkvE6DHZz6iRnh9DC5sEnSxEVb9gHdnf4p4Buq4UjdM1FUwLNypGQmwnpl&show_text=true&width=500" width="500" height="997" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fposts%2Fpfbid0wcK4FMVBkvE6DHZz6iRnh9DC5sEnSxEVb9gHdnf4p4Buq4UjdM1FUwLNypGQmwnpl&show_text=true&width=500"width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
       <p class="sermon-date"> October 27, 2025</p>`
 },{
-    facebookVid: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fposts%2Fpfbid0jVKu8nFuqoXGE4G12Y7Lk5GqgRm7YUs5xn7rGXsB7HLb7Ccsjr5a3pTwkxeqkH2Pl&show_text=true&width=500" width="500" height="978" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-      <p class="sermon-date"> October 24, 2025</p>`
+    facebookVid: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fposts%2Fpfbid0jVKu8nFuqoXGE4G12Y7Lk5GqgRm7YUs5xn7rGXsB7HLb7Ccsjr5a3pTwkxeqkH2Pl&show_text=true&width=500" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+      <p class="sermon-date"> October 27, 2025</p>`
 },{
     facebookVid: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F849180147457433%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
       <p class="sermon-date"> October 24, 2025</p>`
