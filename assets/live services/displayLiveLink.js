a8d084132b99bfc622f1e56388c003fc96a748e7
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-10-31 05:47:20 +0000

    Update displayLiveLink.js

--- assets/live services/displayLiveLink.js
+++ assets/live services/displayLiveLink.js
@@ -1,13 +1,22 @@
 export const liveLink = [{
-    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.overah.75%2Fvideos%2F1748316185831012%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+    link: `<iframe 
+        id="liveFrame"
+        src="https://www.facebook.com/plugins/video.php?href=YOUR_FACEBOOK_LIVE_LINK_HERE&show_text=false&autoplay=true"
+        width="100%"
+        height="450"
+        style="border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+      </iframe>
 
 
 
 
-  <div id="fallback-message" 
-       style="display:block; text-align:center; padding:40px; background:#f8f8f8; color:#333; font-size:18px; border-radius:10px;">
-    Live Service will be online on the 26th of October Sunday
-  </div>`
+  <div id="liveFallback" class="live-fallback" style="display: none;">
+        <h3>Live service will be open on Sundays.</h3>
+      </div>`
 }]
 
 
