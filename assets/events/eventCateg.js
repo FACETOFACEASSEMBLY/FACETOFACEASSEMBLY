3623b1bc4dedaf0f4abb5fbd2acfb68f16ce4e1c
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-05 12:14:47 +0000

    Update eventCateg.js

--- assets/events/eventCateg.js
+++ assets/events/eventCateg.js
@@ -3,47 +3,47 @@ export const eventCateg = [{
     time: '5:00 PM - 6:00 PM',
     venue: 'Headquarters Every Monday - Bring your prayer Requests',
     date: {
-        day: 24,
-        month: 'NOV'
+        day: 1,
+        month: 'DEC'
     }
 } ,{
     header: `Tuesday Morning Prayer`,
     time: '5:00 AM - 6:00 AM',
     venue: 'Headquarters Every Tuesday - Bring your prayer Requests',
     date: {
-        day: 25,
-        month: 'NOV'
+        day: 2,
+        month: 'DEC'
     }
 } ,{
     header: `Morning Prayer`,
     time: '5:00 PM - 6:00 PM',
     venue: 'Headquarters Every Wednesday - Bring your prayer Requests',
     date: {
-        day: 26,
-        month: 'NOV'
+        day: 3,
+        month: 'DEC'
     }
 } ,{
     header: `Morning Prayer`,
     time: '6:00 AM - 7:00 AM',
     venue: 'Headquarters Thursday - Bring your prayer Requests',
     date: {
-        day: 27,
-        month: 'NOV'
+        day: 4,
+        month: 'DEC'
     }
 } ,{
     header: `Morning Prayer`,
     time: '6:00 AM - 7:00 AM',
     venue: 'Headquarters Saturday - Bring your prayer Requests',
     date: {
-        day: 29,
-        month: 'NOV'
+        day: 6,
+        month: 'DEC'
     }
 }, {
     header: `Sunday Service`,
     time: '6:30 AM - 8:30 AM | 8:30 AM - 10:30 AM ',
     venue: 'Headquarters Saturday - Bring your prayer Requests',
     date: {
-        day: 30,
-        month: 'NOV'
+        day: 7,
+        month: 'DEC'
     }
 }]
