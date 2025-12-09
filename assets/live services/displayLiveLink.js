418e825fa33f9f95f97ad070ba5ab9ccf4a8eb9e
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-09 09:06:50 +0000

    Update displayLiveLink.js

--- assets/live services/displayLiveLink.js
+++ assets/live services/displayLiveLink.js
@@ -1,6 +1,65 @@
 export const liveLink = [{
     link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Fhappy.word.52%2Fvideos%2F1614554689539765%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
 
+    <div class="lyrics-section">
+  <h2 class="lyrics-title">Alleluia Worship Lyrics</h2>
+
+  <div class="lyrics-box">
+    <pre class="lyrics-text">
+
+Verse 1
+Alleluia, Alleluia,
+For the Lord God Almighty reigns.
+Alleluia, Alleluia!
+For the Lord God Almighty reigns.
+Alleluia!
+
+Holy!
+
+Chorus
+Holy are You, Lord God Almighty!
+Worthy is the Lamb!
+Worthy is the Lamb!
+
+You are holy!
+
+Holy are You, Lord God Almighty!
+Worthy is the Lamb!
+Worthy is the Lamb!
+Amen.
+
+Verse 2
+Alleluia, Alleluia,
+For the Lord God Almighty reigns.
+Alleluia, Alleluia!
+For the Lord God Almighty reigns.
+Alleluia!
+
+Holy!
+
+Chorus
+Holy are You, Lord God Almighty!
+Worthy is the Lamb!
+Worthy is the Lamb!
+
+You are holy!
+
+Holy are You, Lord God Almighty!
+Worthy is the Lamb!
+Worthy is the Lamb!
+
+You are holy!
+
+Chorus
+
+Holy are You, Lord God Almighty!
+Worthy is the Lamb!
+Worthy is the Lamb!
+Amen.
+    </pre>
+  </div>
+</div>
+
 
   <div id="liveFallback" class="live-fallback" style="display: none;">
         <h3>Live service will be open on Sundays.</h3>
