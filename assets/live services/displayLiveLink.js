3c2f997a4799645bb25fb574891cbac452f956f0
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-13 06:10:22 +0000

    Update displayLiveLink.js

--- assets/live services/displayLiveLink.js
+++ assets/live services/displayLiveLink.js
@@ -1,5 +1,5 @@
 export const liveLink = [{
-    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.word.52%2Fvideos%2F1355629439021606%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fweb.facebook.com%2Fhappy.word.52%2Fvideos%2F714965517926653%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
     <div class="lyrics-section">
   <h2 class="lyrics-title">Alleluia Worship Lyrics</h2>
 
