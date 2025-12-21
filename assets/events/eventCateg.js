135cc82d25066bbafdb0eba9fa05ed6091e23b0e
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-21 04:47:55 +0000

    Update eventCateg.js

--- assets/events/eventCateg.js
+++ assets/events/eventCateg.js
@@ -3,7 +3,7 @@ export const eventCateg = [{
     time: '5:00 PM - 6:00 PM',
     venue: 'Headquarters Every Monday - Bring your prayer Requests',
     date: {
-        day: 15,
+        day: 22,
         month: 'DEC'
     }
 } ,{
@@ -11,7 +11,7 @@ export const eventCateg = [{
     time: '5:00 AM - 6:00 AM',
     venue: 'Headquarters Every Tuesday - Bring your prayer Requests',
     date: {
-        day: 16,
+        day: 23,
         month: 'DEC'
     }
 } ,{
@@ -19,7 +19,7 @@ export const eventCateg = [{
     time: '5:00 PM - 6:00 PM',
     venue: 'Headquarters Every Wednesday - Bring your prayer Requests',
     date: {
-        day: 17,
+        day: 24,
         month: 'DEC'
     }
 } ,{
@@ -27,7 +27,7 @@ export const eventCateg = [{
     time: '6:00 AM - 7:00 AM',
     venue: 'Headquarters Thursday - Bring your prayer Requests',
     date: {
-        day: 18,
+        day: 25,
         month: 'DEC'
     }
 } ,{
@@ -35,7 +35,7 @@ export const eventCateg = [{
     time: '6:00 AM - 7:00 AM',
     venue: 'Headquarters Saturday - Bring your prayer Requests',
     date: {
-        day: 20,
+        day: 27,
         month: 'DEC'
     }
 }, {
@@ -43,7 +43,7 @@ export const eventCateg = [{
     time: '6:30 AM - 8:30 AM | 8:30 AM - 10:30 AM ',
     venue: 'Headquarters Saturday - Bring your prayer Requests',
     date: {
-        day: 21,
+        day: 28,
         month: 'DEC'
     }
 }]
