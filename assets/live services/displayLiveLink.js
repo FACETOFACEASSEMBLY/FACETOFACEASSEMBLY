5b0a923f2008353103cc255f67f2207dcce1856d
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2025-12-22 02:52:19 +0000

    Update displayLiveLink.js

--- assets/live services/displayLiveLink.js
+++ assets/live services/displayLiveLink.js
@@ -1,5 +1,15 @@
 export const liveLink = [{
-    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Ffacetoface.assembly%2Fvideos%2F1542160177051478%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+    link: `<iframe 
+        id="liveFrame"
+        src="https://www.facebook.com/plugins/video.php?href=YOUR_FACEBOOK_LIVE_LINK_HERE&show_text=false&autoplay=true"
+        width="100%"
+        height="450"
+        style="border:none;overflow:hidden"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen="true"
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+      </iframe>
    
 
 
