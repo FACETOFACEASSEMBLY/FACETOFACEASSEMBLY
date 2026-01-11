304df6fc1b3e4e28ce79149e8aede475c66c6077
FACETOFACEASSEMBLY <praiseoverah@gmail.com>
2026-01-11 06:32:56 +0000

    Update displayLiveLink.js

--- assets/live services/displayLiveLink.js
+++ assets/live services/displayLiveLink.js
@@ -1,5 +1,5 @@
 export const liveLink = [{
-    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.word.52%2Fvideos%2F922499610953330%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+    link: `<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Fhappy.word.52%2Fvideos%2F877532868366523%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
    
 
 
